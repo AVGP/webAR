0462e6464dc72df7c4acb6afe519c9f803ee201d
Martin Naumann <martin@geekonaut.de>
2014-11-22 17:10:11 +0100

    Fullscreen, orientation lock and stuff.

--- main.js
+++ main.js
@@ -1,33 +1,59 @@
+// Generic definitions - variables
+
 var WIDTH = document.body.clientWidth, HEIGHT = document.body.clientHeight;
+
+var lockOrientation = screen.orientation.lock || screen.lockOrientation
+                   || screen.mozLockOrientation || screen.msLockOrientation;
+
+navigator.getMedia = ( navigator.getUserMedia ||
+                       navigator.webkitGetUserMedia ||
+                       navigator.mozGetUserMedia ||
+                       navigator.msGetUserMedia);
 
 var scene = new THREE.Scene(),
     camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 1000),
-    renderer = new THREE.WebGLRenderer({ alpha: true });
+    renderer = new THREE.WebGLRenderer({ alpha: true }),
+    light, geometry, north, south, east, west, ctx,
+    canvas = document.getElementById("camera"),
+    video  = document.querySelector("video"),
+    button = document.querySelector("button");
 
-document.body.appendChild(renderer.domElement);
-renderer.setSize(WIDTH, HEIGHT);
+// Generic definitions functions
 
-light = new THREE.PointLight(0xffffff, 1, 100);
-scene.add(light);
+function deg2rad(angle) {
+  return (angle / 180.0) * Math.PI;
+}
 
-var geometry = new THREE.BoxGeometry(10, 10, 10);
-var north = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({color: 0xff0000}));
-var south = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({color: 0x00ff00}));
-var east  = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({color: 0x0000ff}));
-var west  = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({color: 0xffff00}));
+function draw() {
+  ctx.fillStyle = "#ff00ff";
+  ctx.fillRect(0,0,WIDTH,HEIGHT);
+  ctx.drawImage(video, 0, 0, WIDTH, HEIGHT);
 
-north.position.set(0, 0, -50);
-south.position.set(0, 0,  50);
+  north.rotation.y += 0.05;
+  south.rotation.y += 0.05;
 
-east.position.set( 50, 0, 0);
-west.position.set(-50, 0, 0);
+  east.rotation.y += 0.05;
+  west.rotation.y += 0.05;
 
-scene.add(north);
-scene.add(east);
-scene.add(south);
-scene.add(west);
+  renderer.render(scene, camera);
+  requestAnimationFrame(draw);
+}
 
-window.addEventListener("deviceorientation", function(e) {
+function showWebcamVideo(sourceId) {
+  navigator.getMedia({
+    video: {
+      optional: [{sourceId: sourceId}]
+    }
+  }, function onSuccess(stream) {
+    var video = document.querySelector('video');
+    video.src = window.URL.createObjectURL(stream);
+  }, function onError(err) {
+    alert("Whoops: " + err);
+    console.error(err);
+  });
+}
+
+function updateOrientation(e) {
   var heading = e.alpha,
       pitch   = e.gamma;
 
@@ -46,64 +72,62 @@ window.addEventListener("deviceorientation", function(e) {
 
   if(heading < 0) heading = 360 + heading;
 
-  camera.rotation.set(0, deg2rad(heading), 0);
-});
-
-function deg2rad(angle) {
-  return (angle / 180.0) * Math.PI;
+  camera.rotation.set(deg2rad(pitch), deg2rad(heading), 0);
 }
 
-var canvas = document.getElementById("camera"),
-    video  = document.querySelector("video");
+// Initialisiation and run!
 
-canvas.width = WIDTH;
-canvas.height = HEIGHT;
+light = new THREE.PointLight(0xffffff, 1, 100);
+scene.add(light);
 
-var ctx = canvas.getContext("2d");
+geometry = new THREE.BoxGeometry(10, 10, 10);
+north = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({color: 0xff0000}));
+south = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({color: 0x00ff00}));
+east  = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({color: 0x0000ff}));
+west  = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({color: 0xffff00}));
 
-function draw() {
-  ctx.fillStyle = "#ff00ff";
-  ctx.fillRect(0,0,WIDTH,HEIGHT);
-  ctx.drawImage(video, 0, 0, WIDTH, HEIGHT);
+north.position.set(0, 0, -50);
+south.position.set(0, 0,  50);
 
-  north.rotation.y += 0.05;
-  south.rotation.y += 0.05;
+east.position.set( 50, 0, 0);
+west.position.set(-50, 0, 0);
 
-  east.rotation.y += 0.05;
-  west.rotation.y += 0.05;
+scene.add(north);
+scene.add(east);
+scene.add(south);
+scene.add(west);
 
-  renderer.render(scene, camera);
-  requestAnimationFrame(draw);
-}
+window.addEventListener("deviceorientation", updateOrientation);
 
-navigator.getMedia = ( navigator.getUserMedia ||
-                       navigator.webkitGetUserMedia ||
-                       navigator.mozGetUserMedia ||
-                       navigator.msGetUserMedia);
+button.addEventListener("click", function() {
+  canvas.className = "";
 
-MediaStreamTrack.getSources(function(mediaSources) {
-  mediaSources.forEach(function(mediaSource){
-    if (mediaSource.kind === 'video' && mediaSource.facing == "environment") {
-      console.log("media source found...")
-      showWebcamVideo(mediaSource.id);
-    }
+  if(document.body.webkitRequestFullscreen) document.body.webkitRequestFullscreen();
+  else if (document.body.mozRequestFullScreen) document.body.mozRequestFullScreen();
+
+  document.body.removeChild(document.getElementById("instructions"));
+
+  document.body.appendChild(renderer.domElement);
+  renderer.setSize(WIDTH, HEIGHT);
+
+  if(screen.orientation.lock && !screen.orientation.lock('landscape-primary')) {
+    alert("Please put your phone in landscape mode for this demo");
+  } else if(screen.lockOrientation && !screen.lockOrientation('landscape-primary')) {
+    alert("Please put your phone in landscape mode for this demo");
+  }
+
+  canvas.width = WIDTH;
+  canvas.height = HEIGHT;
+
+  ctx = canvas.getContext("2d");
+
+  MediaStreamTrack.getSources(function(mediaSources) {
+    mediaSources.forEach(function(mediaSource){
+      if (mediaSource.kind === 'video' && mediaSource.facing == "environment") {
+        showWebcamVideo(mediaSource.id);
+      }
+    });
   });
+
+  draw();
 });
-
-function showWebcamVideo(sourceId) {
-  console.log("About to start video...");
-  navigator.getMedia({
-    video: {
-      optional: [{sourceId: sourceId}]
-    }
-  }, function onSuccess(stream) {
-    console.log("Starting webcam video");
-    var video = document.querySelector('video');
-    video.src = window.URL.createObjectURL(stream);
-  }, function onError(err) {
-    alert("Whoops: " + err);
-    console.error(err);
-  });
-}
-
-draw();
